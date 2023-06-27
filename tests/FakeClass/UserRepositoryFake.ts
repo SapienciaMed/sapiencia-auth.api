b7bab29a6b722c02f67a4e6df5e6cad3a3b97d9e
Mauricio Betancur <jmbetancur@i4digital.com>
2023-06-27 14:09:43 -0500

    Pruebas unitarias controlador auth - general

--- tests/FakeClass/UserRepositoryFake.ts
+++ tests/FakeClass/UserRepositoryFake.ts
@@ -8,18 +8,46 @@ const userFake: IUser = {
   lastNames: "Paredes",
   typeDocument: "CC",
   numberDocument: "1234567890",
-  password: "1234567890",
+  password:
+    "$argon2id$v=19$t=3,m=4096,p=1$ncu1UWkGyCct9xY5AbZoEw$2sOHZRIeqkwgdEH1ZBS1oGLhqwjPTw5YKdz04CDZhAE",
   userModify: "test",
   dateModify: DateTime.now(),
   userCreate: "test",
   dateCreate: DateTime.now(),
-  email: "JoseParedes@example.com"
+  email: "jmbetancur@i4digital.com",
 };
 
 export class UserRepositoryFake implements IUserRepository {
   changePasswordUser(password: string, id: number): Promise<IUser | null> {
-    throw new Error("Method not implemented.");
+    const list = [{ ...userFake }];
+
+    return new Promise((res) => {
+      const user = list.find((user) => user.id === id);
+
+      if (!user) {
+        return res(null);
+      }
+
+      user.password = password;
+
+      return res(user);
+    });
   }
+
+  getUserByNumberDocument(numberDocument: string): Promise<IUser | null> {
+    const list = [{ ...userFake }];
+
+    return new Promise((res) => {
+      const user = list.find((user) => user.numberDocument === numberDocument);
+
+      if (!user) {
+        return res(null);
+      }
+
+      return res(user);
+    });
+  }
+
   getUserAllowedActions(_userId: number): Promise<string[]> {
     return new Promise((res) => {
       res([]);
@@ -31,22 +59,24 @@ export class UserRepositoryFake implements IUserRepository {
       res(userFake);
     });
   }
+
   updateUser(_user: IUser, _id: number): Promise<IUser | null> {
-    return new Promise((res) => {
-      res(userFake);
-    });
-  }
-  getUserByNumberDocument(_document: string): Promise<IUser | null> {
     return new Promise((res) => {
       res(userFake);
     });
   }
 
   getUserById(id: number): Promise<IUser | null> {
-    const list = [userFake];
+    const list = [{ ...userFake }];
 
     return new Promise((res) => {
-      res(list.find((i) => i.id == id) || null);
+      const user = list.find((user) => user.id === id);
+
+      if (!user) {
+        return res(null);
+      }
+
+      return res(user);
     });
   }
 }
